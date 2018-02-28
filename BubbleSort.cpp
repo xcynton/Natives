da38e2bba2fc63e799e3f49a82bd8eb131fa3d21
Max <mfruehmann@student.tgm.ac.at>
2018-02-28 18:09:27 +0100

    Erweiterung BubbleSort: Long Arrays

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -13,3 +13,16 @@ JNIEXPORT void JNICALL Java_BubbleSort_callnative(JNIEnv *env,  jobject object, 
     }
     return array;
 }
+JNIEXPORT void JNICALL Java_BubbleSort_callnative(JNIEnv *env, jobject object, jlong[] *array, jlong length){
+    long i, j,tmp;
+    for (i = 0; i < length ; i++) {
+        for (j = i; j < length ; j++) {
+            if (array[i] > array[j]) {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+    }
+    return array;
+}
