a8b8d35e46b535454525ed497f565b79c4f92a6b
Max <mfruehmann@student.tgm.ac.at>
2018-02-28 07:39:42 +0100

    Bubble Sort Native

---
+++ BubbleSort.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+
+JNIEXPORT void JNICALL Java_BubbleSort_callnative(JNIEnv *env,  jobject object, jint[] *array, jint length){
+    int i, j,tmp;
+    for (i = 0; i < length ; i++) {
+        for (j = i; j < length ; j++) {
+            if (array[i] > array[j]) {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+    }
+    return array;
+}
